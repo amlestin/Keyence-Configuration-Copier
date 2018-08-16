0a186be861d1a9bc52505dc8c69af57fbbae172a
Arnold Lestin <amlestin@gmail.com>
2018-08-16 15:26:02 -0400

    Working, simplified version

--- Copy_microscope_configuration.py
+++ Copy_microscope_configuration.py
@@ -1,38 +1,12 @@
 import shutil, os
 
-activated_users = os.listdir('C:\\Users\\')
 
-print("Users on this computer:")
-count = 0
-for user in activated_users:
-    print(user)
-print("\n")
+username = os.getlogin()
+settings_folder_path = "C:\\Users\\" + username + "\\AppData\\Local"
 
-username_valid_flag = False
-while (username_valid_flag == False):
-    print("Type in the username you used to login: ")
-    username = input()
-    username_valid_flag = username in activated_users
-
-user_config_directory = "C:\\Users\\" + username + "\\AppData\\Local\\"
-config_directory_contents = os.listdir(user_config_directory)
-
-keyence_folder_exists_flag = "Keyence" in config_directory_contents
-
-keyence_config_directory = user_config_directory + "\\Keyence\\"
-keyence_config_directory = os.path.normpath(keyence_config_directory)
-
-# DELETE ME
-keyence_folder_exists_flag = True
-# DELETE ME
-
-if(keyence_folder_exists_flag == False):
-    print("\n")
-    print("The folder " + keyence_config_directory + " does not yet exist.")
-    print("Please open and close the BZ-X Viewer software on the desktop of your computer in order to continue.")
-    quit()
-    
-master_config_exists_flag = "BZ-X" in os.listdir("C:\\")
+master_config_folder_path = "C:\\BZ-X"
+master_config_exists_flag = os.path.exists(master_config_folder_path)
 
 if master_config_exists_flag == True:
-    shutil.copytree("C:\\BZ-X\\", "C:\\Specialdir\\")
+    shutil.copytree(master_config_folder_path, settings_folder_path + "\\Keyence\\")
+        
