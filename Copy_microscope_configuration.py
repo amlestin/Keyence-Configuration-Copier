a9d4455fb97ff63ee78f45c98d85863c4e816af5
Arnold Lestin <amlestin@gmail.com>
2018-08-20 08:06:00 -0400

    Corrected destination path

--- Copy_microscope_configuration.py
+++ Copy_microscope_configuration.py
@@ -8,5 +8,5 @@ master_config_folder_path = "C:\\BZ-X"
 master_config_exists_flag = os.path.exists(master_config_folder_path)
 
 if master_config_exists_flag == True:
-    shutil.copytree(master_config_folder_path, settings_folder_path + "\\Keyence\\")
+    shutil.copytree(master_config_folder_path, settings_folder_path + "\\Keyence\\BZ-X\\")
         
