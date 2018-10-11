761ecb351fc39432f2651dd8af2e8236a1ab7b2d
Arnold Lestin <amlestin@gmail.com>
2018-10-11 17:53:15 -0400

    Added comments

--- Copy_microscope_configuration.py
+++ Copy_microscope_configuration.py
@@ -1,24 +1,24 @@
 import shutil, os
 
 
-username = os.getlogin()
+username = os.getlogin() # gets the folder name of the logged in user in C:\Users\ folder
 settings_folder_path = "C:\\Users\\" + username + "\\AppData\\Local"
 
 master_config_folder_path = "C:\\BZ-X"
-master_config_exists_flag = os.path.exists(master_config_folder_path)
+master_config_exists_flag = os.path.exists(master_config_folder_path) # checks that there is a folder to write to
 
 if master_config_exists_flag == True:
-    if (os.path.exists(settings_folder_path + "\\Keyence\\")):
+    if (os.path.exists(settings_folder_path + "\\Keyence\\")): # makes backup if the user has previous config files
         error_file = open("ERROR-LOG.txt", "w")
         user_documents_path = "C:\\Users\\" + username + "\\Documents\\Keyence\\"
         
-        if (os.path.exists(user_documents_path)):
-            shutil.rmtree(user_documents_path, ignore_errors=True)
+        if (os.path.exists(user_documents_path)): # deletes the previous backup if it exists
+            shutil.rmtree(user_documents_path, ignore_errors=True) # delete
             
-        shutil.copytree(settings_folder_path + "\\Keyence\\", user_documents_path)
-        shutil.rmtree(settings_folder_path + "\\Keyence\\", ignore_errors=True)
+        shutil.copytree(settings_folder_path + "\\Keyence\\", user_documents_path) # makes backup of user's current config files
+        shutil.rmtree(settings_folder_path + "\\Keyence\\", ignore_errors=True) # deletes the user's current config files
         text = "Old Keyence folder saved at " + user_documents_path
-        error_file.write(text)
-        error_file.close()               
-    shutil.copytree(master_config_folder_path, settings_folder_path + "\\Keyence\\BZ-X\\")
+        error_file.write(text) # writes a file so the user knows their old configuration files were backed up and where
+        error_file.close()
+    shutil.copytree(master_config_folder_path, settings_folder_path + "\\Keyence\\BZ-X\\") # copies the master configuration to this user's account
         
