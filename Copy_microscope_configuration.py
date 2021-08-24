a5f8eace471014430429be9a237b4d061a9e9677
Arnold Lestin <amlestin@gmail.com>
2021-08-24 11:41:23 -0400

    Made script explicit

--- Copy_microscope_configuration.py
+++ Copy_microscope_configuration.py
@@ -1,28 +1,31 @@
 import shutil, os, platform
 
-# only supports Windows
-if platform.system() != "Windows":
-    print("Error: Not a Windows system.")
-    quit()
+def main():
+    # only supports Windows
+    if platform.system() != "Windows":
+        print("Error: Not a Windows system.")
+        quit()
 
-username = os.getlogin() # gets the folder name of the logged in user in C:\Users\ folder
-settings_folder_path = "C:\\Users\\" + username + "\\AppData\\Local"
+    username = os.getlogin() # gets the folder name of the logged in user in C:\Users\ folder
+    settings_folder_path = "C:\\Users\\" + username + "\\AppData\\Local"
 
-master_config_folder_path = "C:\\BZ-X"
-master_config_exists_flag = os.path.exists(master_config_folder_path) # checks that there is a folder to write to
+    master_config_folder_path = "C:\\BZ-X"
+    master_config_exists_flag = os.path.exists(master_config_folder_path) # checks that there is a folder to write to
 
-if master_config_exists_flag == True:
-    if (os.path.exists(settings_folder_path + "\\Keyence\\")): # makes backup if the user has previous config files
-        error_file = open("ERROR-LOG.txt", "w")
-        user_documents_path = "C:\\Users\\" + username + "\\Documents\\Keyence\\"
-        
-        if (os.path.exists(user_documents_path)): # deletes the previous backup if it exists
-            shutil.rmtree(user_documents_path, ignore_errors=True) # delete
+    if master_config_exists_flag == True:
+        if (os.path.exists(settings_folder_path + "\\Keyence\\")): # makes backup if the user has previous config files
+            error_file = open("ERROR-LOG.txt", "w")
+            user_documents_path = "C:\\Users\\" + username + "\\Documents\\Keyence\\"
             
-        shutil.copytree(settings_folder_path + "\\Keyence\\", user_documents_path) # makes backup of user's current config files
-        shutil.rmtree(settings_folder_path + "\\Keyence\\", ignore_errors=True) # deletes the user's current config files
-        text = "Old Keyence folder saved at " + user_documents_path
-        error_file.write(text) # writes a file so the user knows their old configuration files were backed up and where
-        error_file.close()
-    shutil.copytree(master_config_folder_path, settings_folder_path + "\\Keyence\\BZ-X\\") # copies the master configuration to this user's account
-        
+            if (os.path.exists(user_documents_path)): # deletes the previous backup if it exists
+                shutil.rmtree(user_documents_path, ignore_errors=True) # delete
+                
+            shutil.copytree(settings_folder_path + "\\Keyence\\", user_documents_path) # makes backup of user's current config files
+            shutil.rmtree(settings_folder_path + "\\Keyence\\", ignore_errors=True) # deletes the user's current config files
+            text = "Old Keyence folder saved at " + user_documents_path
+            error_file.write(text) # writes a file so the user knows their old configuration files were backed up and where
+            error_file.close()
+        shutil.copytree(master_config_folder_path, settings_folder_path + "\\Keyence\\BZ-X\\") # copies the master configuration to this user's account
+            
+if __name__ == '__main__':
+    main()
